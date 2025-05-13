40e67b636112ab5a204bee9a4fbe146da7cdec58
Thành Hưng <nguyenthanhung1912@gmail.com>
2025-05-13 19:42:31 +0700

    add new

--- data/triplet_dataset.py
+++ data/triplet_dataset.py
@@ -16,34 +16,18 @@ class TripletDataset(Dataset):
         self.transform = transform
         self.data = data_list
 
-        # Organize images by class
-        self.class_to_indices = defaultdict(list)
-        for idx, item in enumerate(self.data):
-            self.class_to_indices[item['label']].append(idx)
-
-        self.labels = list(self.class_to_indices.keys())
-
     def __len__(self):
         return len(self.data)
 
     def __getitem__(self, idx):
-        anchor_item = self.data[idx]
-        anchor_img = self._load_image(anchor_item['path'])
-        anchor_label = anchor_item['label']
+        row = self.df.iloc[idx]
 
-        # Sample a positive from same class (excluding anchor itself)
-        pos_indices = self.class_to_indices[anchor_label]
-        pos_idx = random.choice([i for i in pos_indices if i != idx])
-        positive_item = self.data[pos_idx]
-        positive_img = self._load_image(positive_item['path'])
+        anchor = self._load_image(row["anchor"])
+        positive = self._load_image(row["positive"])
+        negative = self._load_image(row["negative"])
 
-        # Sample a negative from a different class
-        neg_label = random.choice([l for l in self.labels if l != anchor_label])
-        neg_idx = random.choice(self.class_to_indices[neg_label])
-        negative_item = self.data[neg_idx]
-        negative_img = self._load_image(negative_item['path'])
+        return anchor, positive, negative
 
-        return anchor_img, positive_img, negative_img
 
     def _load_image(self, rel_path):
         full_path = os.path.join(self.root_dir, rel_path)
